bed330bca71f48339db60518a88cd9e2af556f6e
Raul Dias <raulgdias97@gmail.com>
2019-02-14 23:48:41 -0200

    Preparação da EntityConfig

---
+++ ProjetoModeloDDD.Domain/Entities/Produto.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ProjetoModeloDDD.Domain.Entities
+{
+    class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public bool Disponivel { get; set; }
+        public int ClienteId { get; set; }
+        public virtual Cliente Cliente { get; set; }
+
+    }
+}
